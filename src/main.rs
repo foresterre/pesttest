29a43509acce20dc3aa7962efa3328e7184be53d
foresterre <garm@ilumeo.com>
2018-07-31 18:46:47 +0200

    Make the build fail, to show the problem.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@
 use pest::Parser;
 
 // Workaround for Rust 2018 preview, "cannot find attribute macro `grammar` in this scope"
-#[macro_use]
-extern crate pest_derive;
+// #[macro_use] extern crate pest_derive;
 
 // https://crates.io/crates/pest crates.io latest => 1.0.6
 // https://github.com/pest-parser/pest/blob/v1.0.6/pest_derive/src/lib.rs#L263
 // https://doc.rust-lang.org/book/first-edition/procedural-macros.html
 // https://rust-lang-nursery.github.io/edition-guide/2018/transitioning/modules/macros.html
 //use pest_derive::derive_parser; // error[E0432]: unresolved import `pest_derive::derive_parser`, no `derive_parser` in the root
-
-
 
 const _GRAMMAR: &str = include_str!("csv.pest");
 
